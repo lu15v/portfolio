215ff3a11dc648e4d5863bbe366e3293f1da0f0c
lu15v <lu15bytes@gmail.com>
2021-09-16 20:02:14 -0700

    FEATURE: basic header component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Curtain from './components/Curtain';
 import MainPage from './components/MainPage';
+import PageWrapper from './components/PageWrapper';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      {sessionStorage.getItem("curtain") ? <MainPage/> : <Curtain/>}
+      {sessionStorage.getItem("curtain") ? 
+        <PageWrapper>
+          <MainPage/>
+        </PageWrapper>
+       : <Curtain/>}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header.scss';
+
+const Header = () =>{
+    return(
+        <header className="site-header">
+            <div className="header-wrapper">
+
+            </div>
+        </header>
+    );
+}
+
+export default Header;
