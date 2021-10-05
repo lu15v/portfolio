3af5fa3202366b7b90e121ac312063d1c21bb018
lu15v <lu15bytes@gmail.com>
2021-10-04 19:20:49 -0700

    FEATURE: load more button added, skeleton styling improved

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,11 +4,26 @@ import SkeletonLoading from './SkeletonLoading';
 import '../styles/portfolio.scss';
 import { withRouter } from 'react-router';
 
+
+const loadMoreButton = (message, callback) =>{
+    return(
+        <button onClick={callback}>{message}</button>
+    )
+}
+
+
 const Portfolio = () =>{
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+
+    const handleClick = () =>{
+        console.log("handleClick")
+    }
 
     return(
-        loading ?(<div className="portfolio-items">
+        !loading ?(
+        <>
+        <div className="portfolio-items">
                 <Project name="webtry backend"/>
                 <Project/>
                 <Project/>
@@ -17,12 +32,24 @@ const Portfolio = () =>{
                 <Project/>
                 <Project/>
                 <Project/>
-            </div>):
-            <div className="portfolio-items">
-                <SkeletonLoading items={6}>
-                    <Project isSkeleton={true}/>
-                </SkeletonLoading>
             </div>
+            <div className="load-more-wrapper">
+                {loadMoreButton("Load More", handleClick)}
+            </div>
+            </>
+            ):
+            <>
+                <div className="portfolio-items">
+                    <SkeletonLoading items={8}>
+                        <Project isSkeleton={true}/>
+                    </SkeletonLoading>
+                </div>
+                <div className="load-more-wrapper">
+                    <SkeletonLoading items={1}>
+                        {loadMoreButton("", )}
+                    </SkeletonLoading>
+                </div>
+            </>
     )
 
 }
