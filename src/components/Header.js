61e21066829938c3de2ee1a07bc1842467493b81
lu15v <lu15bytes@gmail.com>
2021-10-08 01:32:39 -0700

    UPDATE: styling to the navs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import {NavLink, withRouter} from 'react-router-dom';
+import React,{useEffect, useState} from 'react';
+import {NavLink, withRouter, useLocation} from 'react-router-dom';
 
 import picture from '../assets/logo.png';
 
 import '../styles/header.scss';
 
 const Header = ({history}) =>{
-    // const {pathname} = useLocation();
+    const {pathname} = useLocation();
+    const [activePath, setActivePath] = useState('');
 
     const redirectHome = () =>{
         history.push('/');
     }
 
+    useEffect(() => {
+        setActivePath(pathname);
+    }, [pathname])
+    
     return(
         <header className="site-header">
             <div className="header-wrapper">
                 <div className="header-content">
                     <img id="logo"  className="logo" onClick={redirectHome} src={picture} alt="logo"/>
                     <div className="navbar-wrapper">
-                        <NavLink to="/about">About</NavLink>
-                        <NavLink to="/contact">Conctact</NavLink>
-                        <NavLink to="/resume">Resume</NavLink>
+                        <NavLink to="/about">{activePath === '/about' ? '{About}' : "About"}</NavLink>
+                        <NavLink to="/contact">{activePath === '/contact' ? '{Contact}' : "Contact"}</NavLink>
+                        <NavLink to="/resume">{activePath === '/resume' ? '{Resume}' : "Resume"}</NavLink>
                     </div>
                 </div>
             </div>
