ba9de6d4a36271cc7d3b47a7e0e8ffe67a179eec
lu15v <lu15bytes@gmail.com>
2021-10-12 00:03:37 -0700

    FIX: general grid fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
-
+import SquaredLink from './SquaredLink';
+import '../styles/contact.scss';
 
 const Contact = () =>{
 
     return(
-        <div>
-            <h1>Contact</h1>
+        <div id="menu-grid" className="menu-grid-container">
+            <div className="menu-elements">
+                <SquaredLink styles="portfolio" linkTo="/portfolio" name="Portfolio"/>
+                <SquaredLink styles="about" linkTo="/about" name="About"/>
+                <SquaredLink styles="contact" linkTo="/contact" name="Contact"/>
+                <SquaredLink styles="old-site" name="Old Site" url="https://rasanod.com"/>
+                <SquaredLink styles="resume" linkTo="/resume" name="Resume"/>
+            </div>
         </div>
     )
 
