d7bc189c93659ae66442b02442a668b73967aebf
lu15v <lu15bytes@gmail.com>
2021-10-29 13:47:09 -0700

    FEATURE: burger menu component created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React,{useEffect, useState} from 'react';
 import {NavLink, withRouter, useLocation} from 'react-router-dom';
-
+import useWindowDimensions from '../hooks/windowDImentions';
+import BurgerMenu from './burgerMenu';
 import picture from '../assets/logo.png';
-
+import menu from '../assets/icons/burger-icon.png';
 import '../styles/header.scss';
 
 const Header = ({history}) =>{
     const {pathname} = useLocation();
     const [activePath, setActivePath] = useState('');
+    const [displayMenu, setDisplayMenu] = useState(false);
+
+    const { width} = useWindowDimensions()
 
     const redirectHome = () =>{
         history.push('/');
@@ -17,19 +21,28 @@ const Header = ({history}) =>{
         setActivePath(pathname);
     }, [pathname])
     
+
     return(
-        <header className="site-header">
-            <div className="header-wrapper">
-                <div className="header-content">
-                    <img id="logo"  className="logo" onClick={redirectHome} src={picture} alt="logo"/>
-                    <div className="navbar-wrapper">
-                        <NavLink to="/about">{activePath === '/about' ? '{About}' : "About"}</NavLink>
-                        <NavLink to="/contact">{activePath === '/contact' ? '{Contact}' : "Contact"}</NavLink>
-                        <NavLink to="/resume">{activePath === '/resume' ? '{Resume}' : "Resume"}</NavLink>
+        <>
+            {displayMenu && <BurgerMenu callback={setDisplayMenu}/>}
+            <header className="site-header">
+                <div className="header-wrapper">
+                    <div className="header-content">
+                        <img id="logo"  className="logo" onClick={redirectHome} src={picture} alt="logo"/>
+                        <div className="navbar-wrapper">
+                            {width > 900 ? (
+                            <>
+                            <NavLink to="/about">{activePath === '/about' ? '{About}' : "About"}</NavLink>
+                            <NavLink to="/contact">{activePath === '/contact' ? '{Contact}' : "Contact"}</NavLink>
+                            <NavLink to="/resume">{activePath === '/resume' ? '{Resume}' : "Resume"}</NavLink>
+                            </>
+                            ) :
+                            <img id="menu"  className="menu" onClick={() => setDisplayMenu(true)} src={menu} alt="logo"/>}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 }
 
---
+++ src/components/burgerMenu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+import '../styles/burgerMenu.scss';
+import close from '../assets/icons/close.png';
+
+const BurgerMenu = ({callback}) =>{
+
+    return(
+        <div className="burgerMenu-wrapper" onClick={() => callback(false)}>
+            <img id="close" src={close}  onClick={() => callback(false)} alt="close"/>
+            
+            <NavLink id="about" to="/about">About</NavLink>
+            <NavLink id="contact" to="/contact">Contact</NavLink>
+            <NavLink id="resume" to="/resume">Resume</NavLink>
+        </div>
+    )
+}
+
+export default BurgerMenu;
