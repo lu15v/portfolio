7670046ce473ec3576273dac4c1053498b795048
lu15v <lu15bytes@gmail.com>
2021-11-01 21:30:53 -0700

    FEATURE: apollo client setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from "history";
 import {BrowserRouter as Router} from 'react-router-dom';
+import {ApolloClient, InMemoryCache, createHttpLink, ApolloProvider} from '@apollo/client';
 
 import './index.scss';
 import App from './App';
+
 const history = createBrowserHistory();
+
+const httpLink = createHttpLink({
+  uri: 'https://portfolio-graphql-be.herokuapp.com/'
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
-      <App />
-    </Router>
+      <Router history={history}>
+        <ApolloProvider client={apolloClient}>
+          <App />
+        </ApolloProvider>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
