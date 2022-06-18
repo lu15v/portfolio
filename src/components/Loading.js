e7bec4642a88e3e85bca77c326ec59196a2315dc
lu15v <lu15bytes@gmail.com>
2022-06-17 17:41:46 -0700

    CHORE: exposing loadingWaves for further use

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,23 +2,24 @@ import React from "react";
 import "../styles/loading.scss";
 import uniqid from "uniqid";
 
-const Loading = (props) => {
-  const { totalContainers = 0 } = props;
-  const MAX_CONTAINERS_TOTAL = 10;
-  const restrictedContainersTotal = totalContainers <= 10 ? totalContainers : MAX_CONTAINERS_TOTAL;
-
-  const waveElements = (total = 0) => {
-    let waveElementsList = [];
-    for(let container = 0; container < total; container++){
-         waveElementsList.push(<div key={uniqid()} className="wave"></div>);
-    }
-
-    return waveElementsList;
+export const waveElements = (total = 0) => {
+  let waveElementsList = [];
+  for(let container = 0; container < total; container++){
+       waveElementsList.push(<div key={uniqid()} className="wave"></div>);
   }
+
+  return waveElementsList;
+}
+
+const Loading = (props) => {
+  const { totalWaves = 0 } = props;
+  const MAX_WAVES_ALLOWED = 10;
+  const waves = totalWaves <= 10 ? totalWaves : MAX_WAVES_ALLOWED;
+
   return (
     <div className="loading-wrapper">
       <div className="waves-wrapper">
-          {waveElements(restrictedContainersTotal)}
+          {waveElements(waves)}
       </div>
       <p className="loading-message">Loading</p>
     </div>
